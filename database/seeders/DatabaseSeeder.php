73e202a6586815fe6815f13d0a0ee1d2805ed10a
lumas <lumastech@gmail.com>
2025-06-09 00:53:21 +0300

    fixed seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Illuminate\Validation\Rules\In;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,23 +18,25 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->withPersonalTeam()->create();
         // create admin user
-        // User::create([
-        //     'name' => 'Admin User',
-        //     'email' => 'admin@cress.com',
-        //     'phone' => '0987654321',
-        //     'sex' => 'male',
-        //     'role' => 'admin',
-        //     'address' => fake()->optional()->streetAddress(),
-        //     'town' => 'Lusaka',
-        //     'email_verified_at' => now(),
-        //     'password' => Hash::make('#password'),
-        //     'two_factor_secret' => null,
-        //     'two_factor_recovery_codes' => null,
-        //     'remember_token' => Str::random(10),
-        //     'profile_photo_path' => null,
-        //     'current_team_id' => null
-        // ]);
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@cress.com',
+            'phone' => '0987654321',
+            'sex' => 'male',
+            'role' => 'admin',
+            'address' => fake()->optional()->streetAddress(),
+            'town' => 'Lusaka',
+            'email_verified_at' => now(),
+            'password' => Hash::make('#password'),
+            'two_factor_secret' => null,
+            'two_factor_recovery_codes' => null,
+            'remember_token' => Str::random(10),
+            'profile_photo_path' => null,
+            'current_team_id' => null
+        ]);
 
-        // User::factory(39)->create();
+        User::factory(39)->create();
+        (new IncidentSeeder)->run();
+        (new CenterSeeder)->run();
     }
 }
