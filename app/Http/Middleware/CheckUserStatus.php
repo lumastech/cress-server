a904925c346c00f949a3e8e7eb23d332e5e1860f
lumas <lumastech@gmail.com>
2025-06-26 21:54:12 +0200

    feat: Add CheckUserStatus middleware to manage user access based on account status

---
+++ app/Http/Middleware/CheckUserStatus.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Session\Session;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class CheckUserStatus
+{
+    /**
+     * Handle an incoming request.
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $user = Auth::user();
+
+        if ($user && ($user->status !== 'active' && $user->status !== 'pending'  && $user->role !== 'admin')) {
+            $request->session()->flush();
+
+            // Optional: redirect with message based on status
+            $message = match ($user->status) {
+                'suspended' => 'Your account is suspended. Please contact support.',
+                'rejected'  => 'Your account application was rejected.',
+                'pending'  => 'Your account application is pending approval.',
+                'banned'    => 'Your account has been banned.',
+                default     => 'Your account is not active. kindly contact support.',
+            };
+
+            return redirect()->route('login')->withErrors([
+                'username' => $message,
+            ]);
+        }
+
+        // If the user is active or pending, redirect to profile show if current route is not profile.show
+        if ($request->route()->getName() !== 'profile.show' && $request->route()->getName() !== 'logout' && $user && $user->status === 'pending') {
+            return redirect()->route('profile.show')->with('message', 'Please complete your profile by filling in all required fields.');
+        }
+
+
+        return $next($request);
+    }
+}
+
